665b12128880228bb5518dfa37de68b873ea678a
pmatos2000 <pmatos2000@gmail.com>
2021-07-24 22:41:03 -0300

    Aula 002 - Variaveis

---
+++ 002_variaveis/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    int idade_paulo = 28;
+    double saldo_paulo = -500.25;
+    printf("A idade do Paulo é %i\n", idade_paulo);
+    printf("O saldo do Paulo é %f\n", saldo_paulo);
+    return 0;
+}
