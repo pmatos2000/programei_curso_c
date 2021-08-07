73aaf168968779ec72d9cbb60212a515e87e39a3
pmatos2000 <pmatos2000@gmail.com>
2021-08-07 20:30:43 -0300

    Aula 003 tipos basicos e printf

---
+++ 003_tipos_basicos/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    char letra = 'A';
+    int idade_paulo = 28;
+    float temperatura = 20.5f;
+    double saldo_paulo = -500.25;
+
+    printf("%c\n", letra);
+    printf("%i\n", idade_paulo);
+    printf("%f\n", temperatura);
+    printf("%f\n", saldo_paulo);
+
+    return 0;
+}
